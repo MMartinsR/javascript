888b4c704d6eb2dc30df7eb7793c7724c566f7d5
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2022-10-20 23:05:34 +0100

    Aula11 - ex008,009,010 - condições

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,15 @@
+/* Aula 11 - Ex008 - Condições
+    -> Para escrever em tela usando js, usamos o console.log, que executa o comando no console:
+        -> console.log('O console funcionou corretamente')
+    -> Condição simples
+        -> if (vel > 60){
+                console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+            }
+*/
+
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,18 @@
+/* Aula 11 - Ex009 - Condições 2
+    -> Condição composta
+        -> if (pais == 'Brasil') {
+                console.log('Você é Brasileiro(a)!')
+            } else {
+                console.log('Você é Estrangeiro(a)!')
+            }
+*/
+
+var pais = 'França'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro(a)!')
+} else {
+    console.log('Você é Estrangeiro(a)!')
+}
