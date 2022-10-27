a1fe986d2b0b823b333774be740f12dbf69e15ee
MMartinsR <mariana.martins.rosa.mmr@gmail.com>
2022-10-27 17:50:54 +0100

    Aula 13 - repetição while, do while e for

---
+++ aulas/aula13/ambiente-teste.js
@@ -0,0 +1,26 @@
+/** Aula 13
+ *  -> while
+ *  -> do while
+ */
+
+var count = 1
+
+while (count <= 3) {
+    console.log(`Passo while ${count}`)
+    count++
+}
+
+do {
+    console.log(`Passo do while ${count}`)
+    count--
+} while (count >= 1)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
---
+++ aulas/aula14/ambiente-teste.js
@@ -0,0 +1,11 @@
+/** Aula 14
+ *  -> for
+ */
+
+console.log('Vai começar')
+for (var count = 1; count <= 5; count++) {
+    console.log(`Passo for ${count}`)
+}
+console.log('FIM')
+
+
